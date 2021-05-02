074eb6c95951da596de60b377bdea22944c977df
realvaibhav <realvaibhav2002@gmail.com>
2021-05-02 15:41:53 +0530

    april lunchtime 2021

---
+++ codechef/sloop.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std ;
+typedef long long ll ;
+#define MAX 1000000000
+#define endl "\n" 
+
+
+int solution()
+{
+   
+   int trip , song=1 ;
+   cin >> trip >> song ;
+   int ans=(trip/song) ;
+   cout << ans << endl ;
+
+   return 0  ;
+}
+
+
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+
+    int t ;
+    cin >> t ;
+    for (int i = 0; i < t; i++)
+    {
+       solution() ;
+    }
+
+    return 0  ;
+}
