9e05ee6e810a375dc844b08037ed29c8df867217
realvaibhav <realvaibhav2002@gmail.com>
2021-06-11 16:26:39 +0530

    if you can solve this problem means you can solve any cp problem

---
+++ codeforces/again-twenty-five.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std  ;
+typedef long long int ll ;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n ;
+    cin >> n ;
+    cout << "25" << endl ;
+    
+    return 0 ;
+}
