090076558a0556f25a31e5700188259f89f8a2ca
realvaibhav <realvaibhav2002@gmail.com>
2021-06-11 16:35:47 +0530

    this problem required full focus

---
+++ codeforces/is-it-rated.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std ;
+typedef long long int ll ;
+#define MAX 1000000
+#define endl "\n" 
+
+int main()
+{
+   
+    cout << "NO" << endl ;
+   
+    return 0;
+}
