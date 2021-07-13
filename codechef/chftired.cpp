a72aeeef9e6b9835d43a4c7f89612c020de21b77
realvaibhav <realvaibhav2002@gmail.com>
2021-07-13 18:19:01 +0530

    chef and difficult questions

---
+++ codechef/chftired.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std ;
+typedef long long int ll ;
+#define endl "\n" 
+
+
+int solution()
+{
+   
+   int a, b ;
+   cin >> a >> b ;
+   
+   cout << "YES" << endl ;
+
+
+   
+   return 0;
+}
+
+
+int main()
+{
+    int t ;
+    cin >> t ;
+    for (int i = 0; i < t; i++)
+    {
+       solution() ;
+    }
+
+    return 0;
+}
