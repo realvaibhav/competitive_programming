2ffe06fe9065efef418e4eea1bd095bf69305341
realvaibhav <realvaibhav2002@gmail.com>
2021-07-13 18:03:28 +0530

    is it a vowel or consonant

---
+++ codechef/voweltb.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std ;
+typedef long long int ll ;
+#define MAX 1000000000
+#define endl "\n" 
+
+int main()
+{
+   
+   char ch ;
+   cin >> ch ;
+   int res=(int)ch ;
+   if (res==65||res==69||res==73||res==79||res==85)
+   {
+        cout << "Vowel" << endl ;
+   }
+   else
+   {
+       cout << "Consonant" << endl ;
+   }
+   
+    
+   
+    return 0;
+}
