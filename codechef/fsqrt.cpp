965dc5813b660c750c6264daa1fb615ba849cbab
realvaibhav <realvaibhav2002@gmail.com>
2021-07-14 04:53:52 +0530

    problem name: finding square roots

---
+++ codechef/fsqrt.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std ;
+typedef long long int ll ;
+int solution()
+{
+   ll n ;
+   cin >> n ;
+   double d=1 ;
+   d=sqrt(n);
+   cout << round(d) << "\n" ;
+   
+   return 0;
+}
+
+int main()
+{
+    ll t ;
+    cin >> t ;
+    for (ll i = 0; i < t; i++)
+    {
+        solution();
+    }
+
+    return 0;
+}
