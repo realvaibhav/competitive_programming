bc7544ae6208f9b1a330846e384d9f56aeb74923
realvaibhav <realvaibhav2002@gmail.com>
2021-08-19 09:36:39 +0530

    Large Square

---
+++ codechef/xlsquare.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std ;
+typedef long long int ll ;
+#define MAX 1000000000
+#define endl "\n" 
+// for(int i=0 ; i<n ; i++)
+// vector<int> vec 
+// set<int> s 
+
+int solution()
+{ 
+   int n , side ;
+   cin >> n >> side ;
+   int temp=sqrt(n) ;
+   cout << temp*side << endl ;
+
+   
+   return 0 ;
+}
+
+
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+
+   int t ;
+   cin >> t ;
+   for (int i = 0; i < t; i++)
+   {
+      solution() ;
+   }
+
+
+   return 0  ;
+}
