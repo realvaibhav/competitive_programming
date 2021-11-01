5489f5a413f5e73c61c9274638e58ccd63cafaa6
realvaibhav <realvaibhav2002@gmail.com>
2021-11-01 14:05:40 +0530

    google kickstart

--- codechef/johny.cpp
+++ codechef/johny.cpp
@@ -1,50 +1,50 @@
-   #include<bits/stdc++.h>
-   using namespace std ;
-   typedef long long int ll ;
-   #define MAX 1000000000
-   #define endl "\n" 
+    #include<bits/stdc++.h>
+    using namespace std ;
+    typedef long long int ll ;
+    #define MAX 1000000000
+    #define endl "\n" 
 
 
-   int solution()
-   { 
-      int n ;
-      cin >> n ;
-      int arr[n] ;
-      for(int i=0 ; i<n ; i++)
-      {
-         cin >> arr[i]  ;
-      }
-      int k ;
-      cin >> k ;
-      int res=arr[k-1] ;
-      sort(arr,arr+n) ;
-      int ans=0 ;
-      for(int i=0 ; i<n ; i++)
-      {
-          if(arr[i]==res)
-          {
-              ans=i+1 ;
-              break ;
-          }
-      }
-      cout << ans << endl ;
+    int solution()
+    { 
+        int n ;
+        cin >> n ;
+        int arr[n] ;
+        for(int i=0 ; i<n ; i++)
+        {
+            cin >> arr[i]  ;
+        }
+        int k ;
+        cin >> k ;
+        int res=arr[k-1] ;
+        sort(arr,arr+n) ;
+        int ans=0 ;
+        for(int i=0 ; i<n ; i++)
+        {
+            if(arr[i]==res)
+            {
+                ans=i+1 ;
+                break ;
+            }
+        }
+        cout << ans << endl ;
 
 
-      return 0  ;
-   }
+        return 0  ;
+    }
 
 
-   int main()
-   {
-      ios_base::sync_with_stdio(false);
-      cin.tie(NULL);
+    int main()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
 
-      int t ;
-      cin >> t ;
-      for (int i = 0; i < t; i++)
-      {
-         solution() ;
-      }
+        int t ;
+        cin >> t ;
+        for (int i = 0; i < t; i++)
+        {
+            solution() ;
+        }
 
-      return 0  ;
-   }
+        return 0  ;
+    }
--- codechef/redalert.cpp
+++ codechef/redalert.cpp
@@ -1,63 +1,63 @@
-   #include<bits/stdc++.h>
-   using namespace std ;
-   typedef long long int ll ;
-   #define MAX 1000000000
-   #define endl "\n" 
+    #include<bits/stdc++.h>
+    using namespace std ;
+    typedef long long int ll ;
+    #define MAX 1000000000
+    #define endl "\n" 
 
 
-   int solution()
-   { 
-      int n , d , risk_level  ;
-      cin >> n  >> d >> risk_level  ;
-      int arr[n] ;
-      for(int i=0 ; i<n ; i++)
-      {
-         cin >> arr[i]  ;
-      }
-      int water_level=0 ;
-      for(int i=0 ; i<n ; i++)
-      {
-          if(arr[i]==0)
-          {
-              if(water_level>=d)
-              {
-                  water_level-=d ;
-              }
-              else
-              {
-                  water_level=0 ;
-              }
-          }
-          else  // arr[i]>0
-          {
-              water_level+=arr[i] ;
-          }
+    int solution()
+    { 
+        int n , d , risk_level  ;
+        cin >> n  >> d >> risk_level  ;
+        int arr[n] ;
+        for(int i=0 ; i<n ; i++)
+        {
+            cin >> arr[i]  ;
+        }
+        int water_level=0 ;
+        for(int i=0 ; i<n ; i++)
+        {
+            if(arr[i]==0)
+            {
+                if(water_level>=d)
+                {
+                    water_level-=d ;
+                }
+                else
+                {
+                    water_level=0 ;
+                }
+            }
+            else  // arr[i]>0
+            {
+                water_level+=arr[i] ;
+            }
 
-          if(water_level>risk_level)
-          {
-              cout << "YES" << endl ;
-              return 0 ;
-          }
-      }
-      
-      cout << "NO" << endl ;
-
-
-      return 0  ;
-   }
+            if(water_level>risk_level)
+            {
+                cout << "YES" << endl ;
+                return 0 ;
+            }
+        }
+        
+        cout << "NO" << endl ;
 
 
-   int main()
-   {
-      ios_base::sync_with_stdio(false);
-      cin.tie(NULL);
+        return 0  ;
+    }
 
-      int t ;
-      cin >> t ;
-      for (int i = 0; i < t; i++)
-      {
-         solution() ;
-      }
 
-      return 0  ;
-   }
+    int main()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int t ;
+        cin >> t ;
+        for (int i = 0; i < t; i++)
+        {
+            solution() ;
+        }
+
+        return 0  ;
+    }
